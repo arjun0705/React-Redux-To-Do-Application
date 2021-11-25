3f1ab36423fde75516d338b85d92b414c35fdf80
Arjun Samrat <arjunsamrat0705@gmail.com>
2021-11-25 22:41:51 +0530

    updated

---
+++ src/Components/Login.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import { SetLogin } from '../Reducers/Reducers';
+// import {useSelector} from "react-redux";
+
+function Login() {
+    const todo = useSelector(state=>state.TodoApp)
+    const dispatch = useDispatch();
+    const [style,setStyle]=useState({});
+    const [state,setState]=useState({
+        fname:undefined,
+        lname:undefined,
+        email:undefined,
+    })
+    // const todo = useSelector((state) => state.TodoApp)
+    const handleChange=(e)=>{
+        // debugger
+        
+        setStyle({
+            background:"#aa5555ff"
+        });
+        
+        setState({...state,[e.target.name]:e.target.value});
+    }
+        
+    const storeUserData = ()=> {
+        // e.preventDefault()
+        if(state.fname !== "" && state.lname !== "" && state.email !== "" && state.fname !== undefined && state.lname !== undefined && state.email !== undefined){
+            console.log(state);
+            dispatch(SetLogin(state));
+            
+            
+        }
+
+    }
+    return (
+        <div className={todo.isLight?"login-container light":"login-container"}>
+            <form className="login">
+                <div>
+                   
+                    <label htmlFor="fname" style={{color:'black'}}>First Name</label>
+                    <input onChange={handleChange} type="text" style={state.fname===""?style:{}}  value={state.fname} name="fname" id="fname" placeholder="Enter Your First Name Here..." />
+                </div>
+                <div>
+                    <label htmlFor="lname" style={{color:'black'}}>Last Name</label>
+                    <input onChange={handleChange} type="text" style={state.lname===""?style:{}}  value={state.lname} name="lname" id="lname" placeholder="Enter Your Last Name Here..." />
+                </div>
+                <div>
+                    <label htmlFor="email" style={{color:'black'}}>Email Address</label>
+                    <input onChange={handleChange} type="email" style={state.email===""?style:{}}  value={state.email} name="email" id="email" placeholder="Enter Your Email Here..." />
+                </div>
+                <input type="button" className="btn" value="submit" onClick={storeUserData} />
+            </form>
+        </div>
+    )
+}
+
+export default Login
+
+
---
+++ src/Components/TodoNeeds/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
+import { Logout, changeTheme } from '../../Reducers/Reducers';
+function Header() {
+        const todo = useSelector((state) => state.TodoApp)
+        const dispatch = useDispatch();
+        const logout = ()=>{
+            dispatch(Logout())
+        }
+    return (
+        <div className="header">
+            <h1 className="heading">{todo.heading}</h1>
+            <span className="btn userdata">
+                <span style={{textTransform:"capitalize"}}>Name: {todo.fname} {todo.lname}</span>
+                <span>Email: {todo.email}</span>
+            </span>
+            <div>
+            <span className="btn" onClick={logout}>Logout</span>
+            {/* <span className="theme" onClick={()=>dispatch(changeTheme())}>Switch Theme</span> */}
+            </div>
+           
+        </div>
+    )
+}
+
+export default Header;
